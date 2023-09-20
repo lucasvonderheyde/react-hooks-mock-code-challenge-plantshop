f4b60e0944fad4f10907ce06a3cffcb9dbeb710d
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-09-20 10:34:42 -0400

    finished mock challenge

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,13 +2,31 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useState, useEffect } from "react";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [searchPlants, setSearchPlants] = useState("")
+
+
+
+  const filteredPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(searchPlants.toLowerCase())
+  })
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(response => response.json())
+    .then(plantsResponse => setPlants(previousValue => plantsResponse))
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search setSearchPlants={setSearchPlants} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
